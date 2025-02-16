96cd978cb5f8a4e860b5d9613259625634f67764
Shaukat Ali <Shaukatalibans@gmail.com>
2025-02-16 13:19:40 +0500

    Remove baseUrl since DOMAIN from constants.js is being used in other components

--- frontend/src/components/product/ProductCarousel.jsx
+++ frontend/src/components/product/ProductCarousel.jsx
@@ -1,15 +1,17 @@
 import { Link } from "react-router";
 import { Carousel, Image } from "react-bootstrap";
+import { DOMAIN } from "../../constants";
 
 export const ProductCarousel = ({ products }) => {
-    const baseUrl = "http://localhost:5000";
-
     return (
         <Carousel pause="hover" className="bg-primary mb-5">
             {products?.map((product) => (
                 <Carousel.Item key={product._id}>
                     <Link to={`/products/${product._id}`}>
-                        <Image className="product-carousel-img" src={`${baseUrl}/${product.image}`} />
+                        <Image
+                            className="product-carousel-img"
+                            src={`${DOMAIN}/${product.image}`}
+                        />
                     </Link>
                     <Carousel.Caption className="bg-dark bg-opacity-50 start-0 w-100">
                         <h4>
