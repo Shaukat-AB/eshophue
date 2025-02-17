55f4f9a43346b442879abcad1ef8944e2e35a1e7
Shaukat Ali <Shaukatalibans@gmail.com>
2025-02-17 14:53:17 +0500

    Fix next page link in Paginate component

--- frontend/src/components/pagination/Paginate.jsx
+++ frontend/src/components/pagination/Paginate.jsx
@@ -2,17 +2,16 @@ import { Link } from "react-router";
 import { Pagination } from "react-bootstrap";
 
 export const Paginate = ({ pages, page, pathname }) => {
-    
     return (
         <>
             {pages > 1 && (
-                <Pagination className="mt-4">
+                <Pagination className="py-4">
                     {[...Array(pages).keys()].map((p) => (
                         <Pagination.Item
                             as={Link}
                             key={p + 1}
                             active={p + 1 === page}
-                            to={`${pathname}/page/${p + 1}`}
+                            to={`${pathname}?page=${p + 1}`}
                         >
                             {p + 1}
                         </Pagination.Item>
